229c27bee2dd6081acdadab3c359b8542695ded3
Laurie Corrin <laurie.corrin@multiverse.io>
2022-09-08 12:24:20 -0700

    added App and Welcome

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Welcome } from './Welcome';
+
+
+// Render Components in this Component
+export function App() {
+  return <>
+    <Welcome />
+  </>
+}
---
+++ Welcome.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export function Welcome(){
+  return <h5>Welcome</h5>
+
+}

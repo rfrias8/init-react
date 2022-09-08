c04d2902cf0d5fd413ed633ced353c6371b56d39
Laurie Corrin <laurie.corrin@multiverse.io>
2022-09-08 11:09:51 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import reactDom from "react-dom";
+import ReactDOM from "react-dom";
+
+reactDom.render(
+    <h1>
+        This is JSX, not HTML
+    </h1>,
+    document.getElementById("root")
+);

649f3d04c94e1b3e83f07a4435ebade24dfad12d
rfrias8 <rfrias8@gmail.com>
2022-09-08 15:22:30 -0500

    Added new component

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Welcome } from './Welcome';
+import { Name } from './Name';
+
 
 
 // Render Components in this Component
 export function App() {
   return <>
     <Welcome />
+    <Name />
   </>
 }
